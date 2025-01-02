03cf0df68a9147cdc53596270227f28cd6e42c98
ean3ena <ean3ena@yandex.ru>
2025-01-02 16:23:28 +0300

    add model Url

---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,24 @@
+package hexlet.code.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class Url {
+    private Long id;
+    private LocalDateTime createdAt;
+
+    @ToString.Include
+    private String name;
+
+    public Url(String name) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
