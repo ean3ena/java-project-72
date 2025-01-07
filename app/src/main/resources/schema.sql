9c4d8e4db45ae2c27a7ba034658362db97c0a34f
ean3ena <ean3ena@yandex.ru>
2025-01-07 15:49:02 +0300

    refactor

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE urls (
     id BIGSERIAL PRIMARY KEY,
     name varchar(255) NOT NULL UNIQUE,
+    created_at timestamp NOT NULL
+);
+
+CREATE TABLE url_checks (
+    id BIGSERIAL PRIMARY KEY,
+    status_code int NOT NULL,
+    title varchar(255),
+    h1 varchar(255),
+    description text,
+    url_id BIGSERIAL NOT NULL,
     created_at timestamp NOT NULL
 );
