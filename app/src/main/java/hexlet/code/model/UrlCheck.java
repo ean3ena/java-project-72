11eb7c019b08210260c8b3c3b289c96c476c43e5
ean3ena <ean3ena@yandex.ru>
2025-01-07 15:50:53 +0300

    add url check model

---
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -0,0 +1,24 @@
+package hexlet.code.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UrlCheck {
+    private Long id;
+    private int statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private Long urlId;
+    private LocalDateTime createdAt;
+}
